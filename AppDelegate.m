b5feca1ee7f8b6be7ea5a9d2642526091ec49a17
owenWU <owen.wu@ef.com>
2015-06-15 16:04:19 +0800

    Remove Fabric and Crashlytics

--- AppDelegate.m
+++ AppDelegate.m
@@ -7,8 +7,8 @@
 //
 
 #import "AppDelegate.h"
-#import <Fabric/Fabric.h>
-#import <Crashlytics/Crashlytics.h>
+//#import <Fabric/Fabric.h>
+//#import <Crashlytics/Crashlytics.h>
 
 @interface AppDelegate ()
 
@@ -18,7 +18,7 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    [Fabric with:@[CrashlyticsKit]];
+//    [Fabric with:@[CrashlyticsKit]];
 
     // Override point for customization after application launch.
     return YES;
