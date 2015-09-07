a3a9b3f30f3936bc78e2bdebce83eb97654c38e9
owenWU <owen.wu@ef.com>
2015-09-07 10:04:11 +0800

    pod update

--- Build/Intermediates/scanfind.build/Debug-iphonesimulator/scanfind.build/Objects-normal/x86_64/ViewController.d
+++ Build/Intermediates/scanfind.build/Debug-iphonesimulator/scanfind.build/Objects-normal/x86_64/ViewController.d
@@ -2,4 +2,8 @@ dependencies: /Users/owenwu/Documents/scanFinder/ViewController.m \
   /Users/owenwu/Documents/scanFinder/ViewController.h \
   /Users/owenwu/Documents/scanFinder/CPTTabViewController.h \
   /Users/owenwu/Documents/scanFinder/CPTScanQRViewController.h \
-  /Users/owenwu/Documents/scanFinder/CPTLocationViewController.h
+  /Users/owenwu/Documents/scanFinder/CPTLocationViewController.h \
+  /Users/owenwu/Documents/scanFinder/LNPopupController/LNPopupController.h \
+  /Users/owenwu/Documents/scanFinder/LNPopupController/LNPopupController/ModernObjCSupport.h \
+  /Users/owenwu/Documents/scanFinder/LNPopupController/LNPopupController/LNPopupItem.h \
+  /Users/owenwu/Documents/scanFinder/LNPopupController/LNPopupController/UIViewController+LNPopupSupport.h
